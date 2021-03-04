ec39528d5324b93b87a1428a3b0135d3cf162d1d
sravanthi366 <sravanthimca95@gmail.com>
2021-03-04 22:39:54 +0530

    added resume link

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -44,7 +44,7 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="https://resumesravanthi.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1ta_OTzOQWYwCyIMPGJMWnc0t1llnm2Ge/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
